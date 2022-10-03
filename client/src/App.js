6354db4dbe7cb7227711b1d51296ecb15a128f4f
sohan724flyfar <102467622+sohan724flyfar@users.noreply.github.com>
2022-10-03 11:26:16 +0600

    PreFillForm created

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import FileUpload from './components/FileUpload';
-import './App.css';
+import React from "react";
+import FileUpload from "./components/FileUpload";
+import PreFillForm from "./components/PreFillForm";
+import "./App.css";
 
 const App = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" /> React File Upload
     </h4>
 
-    <FileUpload />
+    {/* <FileUpload /> */}
+    <PreFillForm />
   </div>
 );
 
