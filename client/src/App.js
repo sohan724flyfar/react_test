57830bdd81d9203502bcbbff07d78c8ab13c02ea
sohan724flyfar <sohan@flyfarint.com>
2022-10-10 19:52:04 +0600

    file marged

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import FileUpload from "./components/FileUpload";
 import PreFillForm from "./components/PreFillForm";
 import "./App.css";
+import DynamicForm from "./components/DynamicForm/DynamicForm";
 
 const App = () => (
   <div className="container mt-4">
     <h4 className="display-4 text-center mb-4">
-      <i className="fab fa-react" /> React File Upload
+      <i className="fab fa-react" /> React Test
     </h4>
 
     {/* <FileUpload /> */}
-    <PreFillForm />
+    {/* <PreFillForm /> */}
+    {/* todo: Dynamic form */}
+    <DynamicForm />
   </div>
 );
 
---
+++ client/src/components/DynamicForm/DynamicForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DynamicForm = () => {
+  return <div>DynamicForm</div>;
+};
+
+export default DynamicForm;
