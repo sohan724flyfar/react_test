a8a6617f1d3767237a5787c28d2f6f7134d493ea
sohan724flyfar <sohan@flyfarint.com>
2022-10-10 22:40:54 +0600

    added a new component

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,7 @@ import "./App.css";
 import DynamicForm from "./components/DynamicForm/DynamicForm";
 
 const App = () => (
-  <div className="container mt-4">
-    <h4 className="display-4 text-center mb-4">
-      <i className="fab fa-react" /> React Test
-    </h4>
-
+  <div>
     {/* <FileUpload /> */}
     {/* <PreFillForm /> */}
     {/* todo: Dynamic form */}
--- client/src/components/DynamicForm/FormInput.js
+++ client/src/components/DynamicForm/FormInput.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "./FormInput.css";
 
-const FormInput = () => {
-  return <div>FormInput</div>;
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
 };
 
 export default FormInput;
