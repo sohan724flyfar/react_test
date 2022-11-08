9d0da27bcd8a366f7c1bd1452383c36b96eb7e78
sohan724flyfar <sohan@flyfarint.com>
2022-11-08 21:20:15 +0600

    create home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import FileUpload from "./components/FileUpload";
 import PreFillForm from "./components/PreFillForm";
 import "./App.css";
 import DynamicForm from "./components/DynamicForm/DynamicForm";
+import Home from "./components/Home/Home";
 
 const App = () => (
-  <div>
-    {/* <FileUpload /> */}
-    {/* <PreFillForm /> */}
-    {/* todo: Dynamic form */}
-    <DynamicForm />
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="dynamicform" element={<DynamicForm />} />
+      <Route path="fileupload" element={<FileUpload />} />
+      <Route path="prefillform" element={<PreFillForm />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ client/src/components/UpdatePassengerDocument/UpdatePassengerDocument.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdatePassengerDocument = () => {
+  return <div>UpdatePassengerDocument</div>;
+};
+
+export default UpdatePassengerDocument;
