77e108f59875a7ba32ad4cdd7b6dc7ec24733adf
sohan724flyfar <102467622+sohan724flyfar@users.noreply.github.com>
2022-11-16 11:26:44 +0600

    create file new file upload section

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import PreFillForm from "./components/PreFillForm";
 import "./App.css";
 import DynamicForm from "./components/DynamicForm/DynamicForm";
 import Home from "./components/Home/Home";
+import FileUploadComponet from "./components/FileUploadComponent/FileUploadComponet";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="dynamicform" element={<DynamicForm />} />
-      <Route path="fileupload" element={<FileUpload />} />
+      {/* <Route path="fileupload" element={<FileUpload />} /> */}
+      <Route path="fileupload" element={<FileUploadComponet />} />
       <Route path="prefillform" element={<PreFillForm />} />
     </Routes>
   </BrowserRouter>
