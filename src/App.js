70d581b980cfb2376bb46ad804d094a12b5cd961
sohan724flyfar <102467622+sohan724flyfar@users.noreply.github.com>
2023-01-07 15:40:46 +0600

    update React Query

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import "./App.css";
 import DynamicForm from "./components/DynamicForm/DynamicForm";
 import Home from "./components/Home/Home";
 import FileUploadComponet from "./components/FileUploadComponent/FileUploadComponet";
+import { ReactQuery } from "./components/ReactQuery/ReactQuery";
+import { RQSuperHeroesPage } from "./components/ReactQuery/RQSuperHeroesPage";
+import { SuperHeroesPage } from "./components/ReactQuery/SuperHeroesPage";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/rq-home" element={<ReactQuery />} />
+      <Route path="/super-hero" element={<SuperHeroesPage />} />
+      <Route path="/rq-super-heros" element={<RQSuperHeroesPage />} />
       <Route path="dynamicform" element={<DynamicForm />} />
       {/* <Route path="fileupload" element={<FileUpload />} /> */}
       <Route path="fileupload" element={<FileUploadComponet />} />
